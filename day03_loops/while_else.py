3b28a7504332909e70dc4b6c44cf2e246e131414
SHUBHAM VISHWAKARMA <31842463+rocco96@users.noreply.github.com>
2025-07-24 16:13:25 +0530

    while_else.py

---
+++ day03_loops/while_else.py
@@ -0,0 +1,12 @@
+# for_loop_range.py
+for i in range(1, 6):
+    print("Count:", i)
+
+# for_over_string.py
+for char in "Shubham":
+    print(char)
+
+# nested_loop.py
+for i in range(3):
+    for j in range(2):
+        print(f"i={i}, j={j}")
