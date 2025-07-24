85b0c8184498157e60f839a2dc1cbb539d5eebe0
SHUBHAM VISHWAKARMA <31842463+rocco96@users.noreply.github.com>
2025-07-24 16:04:15 +0530

    comparison_logical.py

---
+++ day02_conditions/comparison_logical.py
@@ -0,0 +1,14 @@
+# Comparison
+a, b = 5, 7
+print(a < b)     # True
+print(a == b)    # False
+print(a != b)    # True
+
+# Assignment
+x = 5
+x += 2   # x = x + 2 → 7
+
+# Logical
+print(True and False)  # False
+print(True or False)   # True
+print(not True)        # False
