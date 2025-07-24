f4ba0ac2bd015fb192a5b3b0f21089944e8f2003
SHUBHAM VISHWAKARMA <31842463+rocco96@users.noreply.github.com>
2025-07-24 16:05:23 +0530

    max_of_two.py

---
+++ day02_conditions/max_of_two.py
@@ -0,0 +1,8 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+if a > b:
+    print("A is greater")
+elif b > a:
+    print("B is greater")
+else:
+    print("Both are equal")
