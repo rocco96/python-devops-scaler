382e7a712436295c289bac8eade9c3123e1fa547
SHUBHAM VISHWAKARMA <31842463+rocco96@users.noreply.github.com>
2025-07-24 16:12:35 +0530

    break_continue.py

---
+++ day03_loops/break_continue.py
@@ -0,0 +1,15 @@
+# break_continue.py
+i = 1
+while i <= 10:
+    if i == 5:
+        break
+    print("Number:", i)
+    i += 1
+
+# while_else.py
+i = 1
+while i <= 3:
+    print(i)
+    i += 1
+else:
+    print("Loop ended normally!")
