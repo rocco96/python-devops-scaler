6e0a2fc02ad0097a5991f7a842a75c4e809d5b76
SHUBHAM VISHWAKARMA <31842463+rocco96@users.noreply.github.com>
2025-07-24 16:03:12 +0530

    arithmetic_ops.py

---
+++ day02_conditions/arithmetic_ops.py
@@ -0,0 +1,10 @@
+# Arithmetic
+a = 10
+b = 3
+print(a + b)   # 13
+print(a - b)   # 7
+print(a * b)   # 30
+print(a / b)   # 3.33
+print(a // b)  # 3 (floor division)
+print(a % b)   # 1 (modulo)
+print(a ** b)  # 1000 (power)
