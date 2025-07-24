9edea098300ccc3599aee03c2d8dd8ccd1597c9e
SHUBHAM VISHWAKARMA <31842463+rocco96@users.noreply.github.com>
2025-07-24 16:04:50 +0530

    traffic_light.py

---
+++ day02_conditions/traffic_light.py
@@ -0,0 +1,9 @@
+light = input("Enter light color: ")
+if light == "red":
+    print("Stop")
+elif light == "yellow":
+    print("Wait")
+elif light == "green":
+    print("Go")
+else:
+    print("Invalid color")
